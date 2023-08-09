d68a41b02f2e7055b5bbe86c0c94fe32b4c5015d
zds <1612859460@qq.com>
2023-08-09 20:51:11 +0800

    配置eslint/prettier

--- yk-design-ui/vite.config.ts
+++ yk-design-ui/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }]
+  },
+  server: {
+    host: true,
+    port: 8086,
+    open: true,
+    cors: true
+  }
 })
