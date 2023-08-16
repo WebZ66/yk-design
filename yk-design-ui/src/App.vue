ad2dc15f82a4de683d9e76267bef3da3dd82814b
Zheng DS <13255183458zds@FT>
2023-08-16 15:52:32 +0800

    button

--- yk-design-ui/src/App.vue
+++ yk-design-ui/src/App.vue
@@ -1,8 +1,13 @@
 <template>
-  <div>
+  <div class="test-container">
     <h1>测试</h1>
     <hr />
-    <yk-button></yk-button>
+    <YkButton type="primary" size="s">小型按钮</YkButton>
+    <YkButton type="primary" size="m" :loading="true">加载按钮</YkButton>
+    <YkButton :type="'secondary'" size="m">深色按钮</YkButton>
+    <YkButton :type="'outline'" size="xl">大型按钮</YkButton>
+    <YkButton :type="'primary'" size="m" shape="circle">+</YkButton>
+    <YkButton :type="'secondary'" size="s" shape="square">+</YkButton>
   </div>
 </template>
 
@@ -11,4 +16,8 @@ import { ref, reactive } from 'vue'
 import YkButton from '@/components/Button/src/YkButton.vue'
 </script>
 
-<style scoped></style>
+<style scoped>
+.test-container {
+  padding: 0 100px;
+}
+</style>
--- yk-design-ui/src/components/button/src/YkButton.vue
+++ yk-design-ui/src/components/button/src/YkButton.vue
@@ -1,27 +1,25 @@
 <template>
-  <button :class="ykButtonClass" :disabled="props.loading||props.disabled">
-    <svg viewBox="25 25 50 50">
-      <circle r="20" cy="50" cx="50">123</circle>
-    </svg>
-    <slot name="icon">123</slot>
+  <button :class="ykButtonClass" :disabled="props.loading || props.disabled">
+    <div :class="{ circle: props.loading }"></div>
+    <slot name="icon"></slot>
     <slot></slot>
   </button>
 </template>
 
 <script setup lang="ts">
 import type { ButtonProps } from './type'
-import { ref, reactive, computed, onMounted, watch } from 'vue'
+import { computed } from 'vue'
 defineOptions({
   name: 'YkButton',
 })
-const props=withDefaults(defineProps<ButtonProps>(),{
-  type:'primary',
-  status:'primary',
-  size:'l',
-  shape:'default',
-  long:false,
-  loading:false,
-  disabled:false
+const props = withDefaults(defineProps<ButtonProps>(), {
+  type: 'primary',
+  status: 'primary',
+  size: 's',
+  shape: 'default',
+  long: false,
+  loading: false,
+  disabled: false,
 })
 const ykButtonClass = computed(() => {
   return {
@@ -29,16 +27,14 @@ const ykButtonClass = computed(() => {
     'yk-button--loading': props.loading,
     'yk-button--long': props.long,
     'yk-button--disabled': props.disabled || props.loading,
-        [`yk-button--${props.status}`]: props.status,
-        [`yk-button--${props.type}`]: props.type,
-        [`yk-button--${props.size}`]: props.size,
-        [`yk-button--${props.shape}`]: props.shape,
-
+    [`yk-button--${props.status}`]: props.status,
+    [`yk-button--${props.type}`]: props.type,
+    [`yk-button--${props.size}`]: props.size,
+    [`yk-button--${props.shape}`]: props.shape,
   }
 })
 </script>
 
 <style lang="scss" scoped>
 @import '../style/index.scss';
-
 </style>
