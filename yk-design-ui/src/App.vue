51c5c3929fd7337dbd8abea1164b0c9209324c3c
Zheng DS <13255183458zds@FT>
2023-08-16 16:00:55 +0800

    更新

--- yk-design-ui/src/App.vue
+++ yk-design-ui/src/App.vue
@@ -3,11 +3,20 @@
     <h1>测试</h1>
     <hr />
     <YkButton type="primary" size="s">小型按钮</YkButton>
+    <span :style="{ 'margin-right': '20px' }"></span>
     <YkButton type="primary" size="m" :loading="true">加载按钮</YkButton>
+    <span :style="{ 'margin-right': '20px' }"></span>
     <YkButton :type="'secondary'" size="m">深色按钮</YkButton>
+    <span :style="{ 'margin-right': '20px' }"></span>
     <YkButton :type="'outline'" size="xl">大型按钮</YkButton>
+    <span :style="{ 'margin-right': '20px' }"></span>
     <YkButton :type="'primary'" size="m" shape="circle">+</YkButton>
-    <YkButton :type="'secondary'" size="s" shape="square">+</YkButton>
+    <span :style="{ 'margin-right': '20px' }"></span>
+    <YkButton :type="'secondary'" size="s" shape="radius">
+      <template #icon>
+        <YkIcon name="saoma" color="#fff"></YkIcon>
+      </template>
+    </YkButton>
   </div>
 </template>
 
