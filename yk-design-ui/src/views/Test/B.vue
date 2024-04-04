5b6d1020c3bd4c7f4d23e03878a940e895f5b535
zds <1612859460@qq.com>
2024-04-04 23:57:25 +0800

    暂存

--- yk-design-ui/src/views/Test/B.vue
+++ yk-design-ui/src/views/Test/B.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    我是B组件
+    我是B组件 {{ props.modelValue }}
+    <button @click="handleClick">+</button>
     <hr />
     <div>B组件的插槽内容</div>
     <slot></slot>
@@ -9,13 +10,15 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, reactive, provide, toRefs } from 'vue'
-const props = defineProps(['modelValue'])
+import { ref, reactive, provide, toRefs, isShallow } from 'vue'
+let props = defineProps(['modelValue'])
 const emits = defineEmits(['update:modelValue', 'change'])
-
+console.log(isShallow(props))
 const radioGroupProps = reactive({
   isGroup: true,
 })
+
+const handleClick = () => {}
 
 //radio-group传递给单个radio的必须是radio-group接收到的那个props对象，如果你重新创建一个，那么provide又不会重新调用
 provide('radioGroupProps', props)
