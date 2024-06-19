7a233173ace76495e384fea3d13c0d1e27617ab6
zhengdashun <13255183458zds@FT>
2024-06-19 17:04:45 +0800

    fix:实现x轴箭头定位

--- yk-design-ui/src/components/popover/src/popover.vue
+++ yk-design-ui/src/components/popover/src/popover.vue
@@ -1,16 +1,69 @@
-<template>
-  <button ref="reference">Button</button>
-  <div ref="floating" :style="floatingStyles">Tooltip</div>
-</template>
+<script setup>
+import { arrow, useFloating, offset, flip, shift } from '@floating-ui/vue'
+import { onMounted, ref, watch } from 'vue'
 
-<script lang="ts" setup>
-import { ref, reactive } from 'vue'
-import { useFloating, arrow } from '@floating-ui/vue'
 const reference = ref(null)
 const floating = ref(null)
-const { floatingStyles } = useFloating(reference, floating, {
-  placement: 'right',
+const floatingArrow = ref(null)
+
+const { floatingStyles, middlewareData } = useFloating(reference, floating, {
+  placement: 'top-start',
+  middleware: [offset(10), flip(), arrow({ element: floatingArrow })],
 })
+
+watch(
+  middlewareData,
+  (newValue) => {
+    console.log(floating.value.offsetHeight)
+    console.log('newValue', newValue)
+  },
+  { deep: true }
+)
 </script>
 
-<style scoped></style>
+<template>
+  <div class="main">
+    <span ref="reference">Reference</span>
+    <div ref="floating" :style="floatingStyles">
+      <div
+        ref="floatingArrow"
+        class="arrow"
+        :style="{
+          position: 'absolute',
+          left:
+            middlewareData.arrow?.x != null
+              ? `${middlewareData.arrow.x}px`
+              : '',
+          top: `70px`,
+        }"
+      ></div>
+      <div class="content">
+        <div class="c"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.main {
+  position: absolute;
+  top: 30%;
+  left: 25%;
+}
+.content {
+  width: 100px;
+  height: 80px;
+  box-sizing: border-box;
+  padding: 10px;
+}
+.c {
+  height: 100%;
+  border: 1px solid black;
+  box-sizing: border-box;
+}
+.arrow {
+  width: 10px;
+  height: 10px;
+  background-color: red;
+}
+</style>
