f6c72d08c4801623869e0079c212c87d7386563d
zhengdashun <13255183458zds@FT>
2024-06-19 16:13:52 +0800

    feat:安装配置floating-ui

--- yk-design-ui/src/components/popover/src/popover.vue
+++ yk-design-ui/src/components/popover/src/popover.vue
@@ -1,11 +1,16 @@
 <template>
-  <Popper>
-    <div class="popper">Popper Content</div>
-  </Popper>
+  <button ref="reference">Button</button>
+  <div ref="floating" :style="floatingStyles">Tooltip</div>
 </template>
 
 <script lang="ts" setup>
 import { ref, reactive } from 'vue'
+import { useFloating, arrow } from '@floating-ui/vue'
+const reference = ref(null)
+const floating = ref(null)
+const { floatingStyles } = useFloating(reference, floating, {
+  placement: 'right',
+})
 </script>
 
 <style scoped></style>
