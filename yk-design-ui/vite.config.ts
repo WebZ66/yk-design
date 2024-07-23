dee95ace17dc4e64b7eafda0d4a2e9af374ae8e2
zhengdashun <13255183458zds@FT>
2024-07-23 16:23:07 +0800

    fix:修复代码

--- yk-design-ui/vite.config.ts
+++ yk-design-ui/vite.config.ts
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
-import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
