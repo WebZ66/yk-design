700062e0a4acb4bb6aa4ce183b2f5b2b2021b614
zhengdashun <13255183458zds@FT>
2024-07-23 16:48:04 +0800

    feat:添加message全局提示

--- yk-design-ui/src/packages/global.d.ts
+++ yk-design-ui/src/packages/global.d.ts
@@ -2,6 +2,8 @@ import { YkButton } from '@/components/button/src/index'
 import { YkSwitch } from '@/components/switch/src'
 import { YkRadio } from '@/components/radio/src'
 import { YkCheckbox } from '@/components/checkbox/src'
+import { YkInput } from '@/components/input/src'
+import Message from '@/components/message/src/methods'
 
 declare module 'vue' {
   /* 扩展全局组件接口类型 */
@@ -10,5 +12,7 @@ declare module 'vue' {
     YkRadio: typeof YkRadio
     YkSwitch: typeof YkSwitch
     YkButton: typeof YkButton
+    YkInput: typeof YkInput
+    Message: typeof Message
   }
 }
--- yk-design-ui/src/packages/index.ts
+++ yk-design-ui/src/packages/index.ts
@@ -1,4 +1,7 @@
 import { YkButton } from '../components/button/src/index'
 import { YkSwitch } from '@/components/switch/src'
 import { YkCheckbox } from '@/components/checkbox/src'
-export { YkButton, YkSwitch, YkCheckbox }
+import { YkInput } from '@/components/input/src'
+import Message from '@/components/message/src/methods'
+
+export { YkButton, YkSwitch, YkCheckbox, YkInput, Message }
