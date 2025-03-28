eb10d92e8a5c3b30869cb39bc931406dec815f09
zhengdashun <zhengdashun@maoyan.com>
2025-03-28 12:02:13 +0800

    feat:创建count-down目录结构

---
+++ yk-design-ui/src/components/count-down/src/count-down.vue
@@ -0,0 +1,12 @@
+<template>
+  <div></div>
+</template>
+
+<script setup lang="ts">
+import { ref, reactive, computed, onMounted, watch } from 'vue'
+defineOptions({
+  name: 'YkCountDown',
+})
+</script>
+
+<style scoped></style>
